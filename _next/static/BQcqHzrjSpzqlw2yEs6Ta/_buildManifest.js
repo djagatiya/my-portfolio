f2e0c7ecfcca0f4ab9d71e42eb9b8e55b7f19e82
djagatiya <divyeshjagatiya@gmail.com>
2025-10-20 16:11:53 +0530

    Updates

---
+++ _next/static/BQcqHzrjSpzqlw2yEs6Ta/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/7a3dbf19e6dd5c5e.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/my-portfolio//_next/:path+",
+        "destination": "/my-portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
